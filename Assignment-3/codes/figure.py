3bae91e7b63846d6eed7036a6c2cfa1483d3a355
vaibhavchhabra25 <80506987+vaibhavchhabra25@users.noreply.github.com>
2021-08-25 17:01:49 +0530

    Update figure.py

--- Assignment-3/codes/figure.py
+++ Assignment-3/codes/figure.py
@@ -23,16 +23,18 @@ def circ_gen(C,r):
 
 # Plotting a random circle for example
 C = np.array([4,5])
-r = 4
-# Taking random x1 and x2 for illustration
-x1 = np.array([C[0]+r*np.cos(1),C[1]+r*np.sin(1)])
-x2 = np.array([C[0]+r*np.cos(2.5),C[1]+r*np.sin(2.5)])
+c = 5
+r = np.sqrt(C[0]**2+C[1]**2-c)
+
+# Taking random x1 and x2 on Circle for illustration
+x1 = np.array([0,5-2*np.sqrt(5)])
+x2 = np.array([-2,5])
 
 M = (x1+x2)/2
 
 ss_mat = np.array([[0,1],[-1,0]])
 
-k = M + (x2-x1)@ss_mat
+k = M - 2*(x2-x1)@ss_mat
 
 x1x2 = line_gen(x1,x2)
 Mk = line_gen(M,k)
@@ -48,13 +50,13 @@ plt.plot(x_circ[0,:],x_circ[1,:],label='circle')
 
 # plotting the points
 plt.plot(C[0],C[1],'go',label='centre C')
-plt.text(C[0],C[1]+0.2,'C')
+plt.text(C[0]-1,C[1]+0.5,'C(4,5)')
 plt.plot(x1[0],x1[1],'bo')
-plt.text(x1[0],x1[1]+0.2,'x1')
+plt.text(x1[0]+0.2,x1[1]+0.5,'x1')
 plt.plot(x2[0],x2[1],'bo')
-plt.text(x2[0]-0.4,x2[1]+0.2,'x2')
+plt.text(x2[0]+0.2,x2[1]+0.5,'x2')
 plt.plot(M[0],M[1],'bo')
-plt.text(M[0],M[1]+0.2,'M')
+plt.text(M[0],M[1]+0.5,'M')
 
 #plotting the lines
 plt.plot(x1x2[0,:],x1x2[1,:],'-',label='Chord')
